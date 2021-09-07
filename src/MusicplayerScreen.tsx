8124888f89040293d3810b52ad6f9a4cb8210009
gauravroy <rockingroy2009@gmail.com>
2021-09-07 22:52:12 +0530

    added pause

--- src/MusicplayerScreen.tsx
+++ src/MusicplayerScreen.tsx
@@ -13,6 +13,7 @@ import {
   FlatList,
   Image,
   Button,
+  Dimensions,
 } from 'react-native';
 import {Bar} from 'react-native-progress';
 
@@ -69,12 +70,24 @@ const MusicPlayer = props => {
     }, 1000);
   };
 
+  const onPauseResume = () => {
+    if (isPaused) {
+      handlePause();
+    } else {
+      handleResume();
+    }
+  };
+
   const handlePause = () => {
     clearInterval(countRef.current);
     setIsPaused(false);
+    const {MusicPlayerModule} = NativeModules;
+    const result = MusicPlayerModule.pauseSong();
   };
 
   const handleResume = () => {
+    const {MusicPlayerModule} = NativeModules;
+    const result = MusicPlayerModule.pauseSong();
     setIsPaused(true);
     countRef.current = setInterval(() => {
       setTimer(timer => timer + 1);
@@ -92,18 +105,36 @@ const MusicPlayer = props => {
 
   const progressOfBar = timer / (duration / 1000) ?? 0;
   console.log(progressOfBar, timer, duration, 'commm');
+
+  const pauseText = isPaused ? 'Pause' : 'Resume';
   return (
     <View style={styles.mainType}>
-      <Text>Music player</Text>
-      <Text>{songName}</Text>
+      <View style={styles.musicW}>
+        <Image
+          source={{uri: 'https://source.unsplash.com/featured/?music'}}
+          style={styles.musicWrap}
+        />
+      </View>
+      <Text style={styles.textSty}>{songName}</Text>
       <View style={styles.muiscTime}>
         <Bar
-          width={150}
-          height={20}
+          style={{overflow: 'hidden', height: 5}}
+          width={Dimensions.get('window').width - 100}
+          height={5}
           progress={progressOfBar}
           useNativeDriver={true}
+          color={'#842df7'}
         />
-        <Text>{momentTime}</Text>
+        <Text style={styles.textSty2}>{momentTime}</Text>
+      </View>
+      <View style={styles.wras}>
+        <Text style={styles.prevText} onPress={onPauseResume}>
+          Previous
+        </Text>
+        <Text style={styles.pauseTex} onPress={onPauseResume}>
+          {pauseText}
+        </Text>
+        <Text style={styles.prevText}>Next</Text>
       </View>
     </View>
   );
@@ -112,12 +143,69 @@ const MusicPlayer = props => {
 const styles = StyleSheet.create({
   mainType: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: '#95a888',
+    // justifyContent: 'center',
+    // alignItems: 'center',
   },
   muiscTime: {
     flexDirection: 'row',
     marginTop: 40,
+    marginHorizontal: 20,
+    alignItems: 'center',
+    // flex: 1,
+  },
+  musicWrap: {
+    height: 200,
+    width: '100%',
+    borderRadius: 20,
+  },
+  musicW: {
+    marginHorizontal: 20,
+    marginVertical: 50,
+    borderRadius: 20,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 4.65,
+
+    elevation: 14,
+  },
+  textSty: {
+    textAlign: 'center',
+    marginHorizontal: 20,
+    fontSize: 14,
+    fontWeight: '700',
+    color: '#062d47',
+  },
+  textSty2: {
+    textAlign: 'center',
+    marginLeft: 20,
+    fontSize: 12,
+    fontWeight: '700',
+    color: '#062d47',
+  },
+  prevText: {
+    textAlign: 'center',
+    marginLeft: 20,
+    fontSize: 12,
+    fontWeight: '700',
+    color: '#062d47',
+  },
+  pauseTex: {
+    textAlign: 'center',
+    marginLeft: 20,
+    fontSize: 12,
+    fontWeight: '700',
+    color: '#2c8a45',
+  },
+  wras: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: 20,
+    marginTop: 30,
   },
 });
 
